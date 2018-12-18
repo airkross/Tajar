c836aea838ad8259761e6664fa2c6aa50c5400c0
Ruslan Guseinov <airkross123@gmail.com>
2018-12-18 12:50:18 +0300

    completed header and add section our-story

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// появление навбара
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".fixed-top");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height() - 10);
+	});
+});
